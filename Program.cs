d45316d44baef61da57020b8a77e8e46acf4cb6f
maciejpajkowski <32037952+maciejpajkowski@users.noreply.github.com>
2019-05-21 14:04:11 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using ConsoleTODO.Models;
+using ConsoleTODO.Views;
+using ConsoleTODO.Controllers;
+
+namespace ConsoleTODO
+{
+    class Program
+    {
+        private const string dataFile = "TasksData.csv";
+        private const string testDataFile = "TestingData.csv";
+        
+        static void Main(string[] args)
+        {
+            int currentOption = 0; 
+
+            Console.Clear();
+            Console.WriteLine("Welcome!");
+
+            // STATE
+            List<TaskModel> tasks = dataFile.FullFilePath().LoadDataFromFile().BuildTaskList();
+            TaskList.Display(tasks);
+
+            while (true) {
+                Menu.Display();
+                Console.Write("What would you like to do? (enter option number): ");
+
+                try {
+                    currentOption = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Returning to main menu...");
+                    continue;
+                }
+
+                switch (currentOption)
+                {
+                    case 1:
+                        DataAccess.CreateNewTask(tasks, dataFile);
+                        TaskList.Display(tasks);
+                        break;
+                    case 2:
+                        DataAccess.EditTask(tasks, dataFile);
+                        TaskList.Display(tasks);
+                        break;
+                    case 3:
+                        DataAccess.RemoveTask(tasks, dataFile);
+                        TaskList.Display(tasks);
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        }
+    }
+}
